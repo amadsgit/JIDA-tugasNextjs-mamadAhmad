2dcb0c261953bc8c72b00fcdf891d1bc6ee96acc
Mamad Ahmad <mamadahmadfnc@gmail.com>
2025-07-17 16:15:24 +0700

    commit pertama

--- app/lib/data-imunisasi.ts
+++ app/lib/data-imunisasi.ts
@@ -1,10 +1,15 @@
-// lib/imunisasi.ts
 import { type DataImunisasi } from '@/app/ui/dashboard/cakupan-imunisasi-chart';
 
 export async function getDataImunisasi(): Promise<DataImunisasi[]> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
+
   const res = await fetch(`${baseUrl}/api/data-imunisasi`, {
     cache: 'no-store',
   });
+
+  if (!res.ok) {
+    throw new Error('Gagal mengambil data imunisasi');
+  }
+
   return res.json();
 }
