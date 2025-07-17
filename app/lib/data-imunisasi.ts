587c80926a8556211a104d5baf632845847a9cb2
Mamad Ahmad <mamadahmadfnc@gmail.com>
2025-07-17 16:45:44 +0700

    commit pertama

--- app/lib/data-imunisasi.ts
+++ app/lib/data-imunisasi.ts
@@ -2,7 +2,7 @@ import { type DataImunisasi } from '@/app/ui/dashboard/cakupan-imunisasi-chart';
 
 export async function getDataImunisasi(): Promise<DataImunisasi[]> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
-  console.log("🚀 BASE URL:", baseUrl); // <--- cek di log Vercel setelah redeploy
+  console.log("BASE URL:", baseUrl);
   const res = await fetch(`${baseUrl}/api/data-imunisasi`, {
     cache: 'no-store',
   });
